ed325b7d4457ab3903f4f174184c07d281e98dc2
JuSellier <juju.sel.mail@gmail.com>
2021-03-01 11:24:23 +0100

    removed console.log and empty css ruleset

--- src/components/PaletteList.svelte
+++ src/components/PaletteList.svelte
@@ -2,13 +2,9 @@
   import Palette from "./Palette.svelte";
 
   export let palettes;
-  console.log(palettes);
 </script>
 
 <style>
-  section.top {
-  }
-
   div {
     --spacing: calc(1rem + 10vh);
     padding: var(--spacing) 0;
